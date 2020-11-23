478073bf0ed7dbca3b6cedb7ea4b87f771bcf013
ganmelLP <ganm@liveperson.com>
2020-11-23 19:15:01 +0000

    dfg

--- bundleNewFaas.js
+++ bundleNewFaas.js
@@ -159,8 +159,8 @@
                 try {
                   console.log("Response from FaaS/Maven: " + parsedBody2);
 
-                  if ((jQuery.isEmptyObject(parsedBody2) == false) && (parsedBody2 == "{}")) {
-                    //console.log("Maven result NOT EMPTY: " + parsedBody2 +", isobjectempty: " + jQuery.isEmptyObject(parsedBody2) + " , is just empty paranthesis: " + (parsedBody2 !== "{}"));
+                  if ((jQuery.isEmptyObject(parsedBody2) == false) && (parsedBody2 !== "{}")) {
+                    console.log("Maven result NOT EMPTY: " + parsedBody2 +", isobjectempty: " + jQuery.isEmptyObject(parsedBody2) + " , is just empty paranthesis: " + (parsedBody2 !== "{}"));
                     var parseda = JSON.parse(parsedBody2);
 
                     const entries = Object.entries(parseda)
